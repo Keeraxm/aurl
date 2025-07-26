b2aa835d7e705fc43c85210aa663f5b223dda6a8
Keera <jesper.tennevik@gmail.com>
2025-07-26 19:18:49 +0200

    deploy ready?

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const { initializeFirebaseApp, postData, getData } = require("./lib/firebase.js");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+initializeFirebaseApp();
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("yello");
+});
+
+app.get("/", (req, res) => {
+    res.send("yello");
+});
+
+app.post("/api/upload/", async (req, res) => {
+    const body = req.body;
+    const docId = await postData(body);
+    res.send(docId);
+});
+
+app.get("/api/retrive/:id", async (req, res) => {
+    const { id }= req.params;
+    const data = await getData(id);
+    res.send(data);
+});
+
+app.listen(PORT, function(err) {
+    if (err) console.logs(err);
+    console.log(`Listning on: ${PORT}`);
+});
